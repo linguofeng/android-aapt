12babfb7916ee1325b34e5e0d27da9efc8f7c402
linguofeng <linguofeng@msn.com>
2015-10-29 00:04:34 +0800

    add linux aapt@6.0

--- install.js
+++ install.js
@@ -1,4 +1,7 @@
-// https://dl-ssl.google.com/android/repository/repository.xml
+// http://pro.sr1.me/post/android-sdk-download-links
+
+// for ubuntu 64-bit
+// sudo apt-get install lib32z1
 
 var https   = require('https');
 var zlib = require('zlib');
@@ -7,12 +10,19 @@ var os = require('os');
 
 var targetDir = __dirname + '/tools/';
 try {
-    fs.statSync(targetDir);
+  fs.statSync(targetDir);
 } catch (e) {
-    fs.mkdirSync(targetDir);
+  fs.mkdirSync(targetDir);
 }
 
-var platform = null; if (os.type() === 'Darwin') { platform = 'macosx'; } else if (os.type() === 'Linux') { platform = 'linux'; } else { throw new Error('Unknown OS!'); }
+var platform = null;
+if (os.type() === 'Darwin') {
+  platform = 'macosx';
+} else if (os.type() === 'Linux') {
+  platform = 'linux';
+} else {
+  throw new Error('Unknown OS!');
+}
 
 var options = {
   hostname  : 'raw.githubusercontent.com',
@@ -24,7 +34,15 @@ var options = {
 var aapt = targetDir + "aapt"
 var file = fs.createWriteStream(aapt);
 
-var req = https.request(options, function(res) { res.on('end', function() { fs.chmodSync(aapt, '755'); }); res .pipe(zlib.createGunzip()) .pipe(file); }); req.end();
+var req = https.request(options, function(res) {
+  res.on('end', function() {
+    fs.chmodSync(aapt, '755');
+  });
+  res
+    .pipe(zlib.createGunzip())
+    .pipe(file);
+});
+req.end();
 
 req.on('error', function(e) {
   console.error(e);
